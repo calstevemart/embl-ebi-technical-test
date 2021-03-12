83960518dc2fe0244d6b08d9450f26bf180c74bf
Callum Martin <callumceltic@gmail.com>
2021-03-12 21:09:48 +0000

    bash script to sanitise an input filename with an optional config file working, commented. Supplied rules.txt file to give example of config format

---
+++ task-3-input-sanitisation/sanitise.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+# Bash script to sanitise input with an optional input file.
+# Written by callum martin as part of the technical test set by the EMBL-EBI
+
+# Grab the filename to be sanitised, and the config file that specifies any replacement characters.
+while getopts i:c: flag
+do
+    case "${flag}" in
+        i) inputString=${OPTARG};;
+        c) config=${OPTARG};;
+    esac
+done
+
+filename=$config
+len=$(echo ${#filename})
+n=1
+# Only try to read if an argument for config has been supplied.
+if [ $len -gt 0 ]
+then
+# Read the config file line by line
+# for each rule, split it into replacee and replacement
+# and then use the stream editor 'sed' utility to perform the replacement
+    while read line; do
+        n=$((n+1))
+        read -a strarr <<< $line
+        replacee="${strarr[0]}"
+        replacement="${strarr[1]}"
+        inputString=$(echo "$inputString" | sed "s/$replacee/$replacement/g")
+    done < $config
+fi
+
+illegalCharacters=("#" "%" "&" "{" "}" "\\" "<" ">" "\*" "?" "/" "" "$" "!" "'" "\"" ":" "@" "+" "\`" "|" "=")
+# For each illegal character, perform the translate 'tr' delete command
+# To delete any instances of that illegal character
+for char in ${illegalCharacters[@]}; do
+    inputString=$(echo "$inputString" | tr -d $char)
+done    
+
+echo "Post sanitisation: $inputString"
