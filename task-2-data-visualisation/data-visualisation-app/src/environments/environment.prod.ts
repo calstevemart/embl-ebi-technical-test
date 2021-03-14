d18102bd70a2e016f672ea08b71afdd5d3bedcc7
Callum Martin <callumceltic@gmail.com>
2021-03-14 21:58:11 +0000

    updated dockerfiles, created production environment file in the ui

--- task-2-data-visualisation/data-visualisation-app/src/environments/environment.prod.ts
+++ task-2-data-visualisation/data-visualisation-app/src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  terminalServerBaseUrl: 'http://3.8.197.227:3000/',
+  machineCalculatorServerBaseUrl: 'http://3.10.20.72:5000/',
 };
