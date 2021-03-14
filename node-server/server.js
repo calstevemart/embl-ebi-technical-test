7bb6f76153e49bea0632b6d437039e1e28222693
Callum Martin <callumceltic@gmail.com>
2021-03-14 00:51:46 +0000

    node server that can run (some) shell commands

---
+++ node-server/server.js
@@ -0,0 +1,38 @@
+
+const express = require('express')
+const cors = require('cors');
+const shell = require('shelljs')
+const bodyParser = require('body-parser')
+
+const app = express()
+const port = 3000;
+
+app.use(cors())
+app.use(bodyParser.json())
+
+app.get('/', (req,res) => {
+    res.json('hit terminal server')
+})
+
+app.post('/sanitise', (req, res) => {
+    console.log(req.body);
+
+   // Build command from flags
+   command = `${req.body.command}`;
+   if(req.body.flags.length > 0){
+    req.body.flags.forEach(flagObject => {
+        command = command.concat(' ' + flagObject.flag + ' ' + flagObject.value)
+    });
+   }
+
+
+   sanitiseCommand = shell.exec(req.body.command);
+   // Oddly this hardcoded command works fine. Unclear as to why.
+   // sanitiseCommand2 = shell.exec('./sanitise.sh -i "hello pal!!"' + ' -c c.txt')
+   res.json(`${sanitiseCommand}`)
+})
+
+
+app.listen(port, function () {
+    console.log(`CORS-enabled web server listening on port ${port}`)
+  })
